37f15462d3894557efebce4ca7e49542b372c2b7
Joshua <j2144970@gmail.com>
2022-03-10 15:30:39 +0800

    123

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world';
+//密碼檢驗
 
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
+var password = 123456;
+var input;
+var deadtime = 0;
+
+while(input!=password && deadtime <=2){
+    input = prompt("請輸入密碼:(還有"+ (3-deadtime) + "次機會)");
+    deadtime +=1;
+
+    if(input == password){
+            alert("登入成功!");
+            break;
+        }else if(deadtime ==3){
+            alert("密碼已鎖住!");
+            break;
+        }
 }
+
+
